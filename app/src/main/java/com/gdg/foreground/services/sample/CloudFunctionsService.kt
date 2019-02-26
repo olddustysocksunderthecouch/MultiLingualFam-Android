60ed47d0d2283ccd29be109db02ce3d7096677a6
Adrian Bunge <adrian@nonacreative.com>
2019-02-27 00:08:57 +0200

    feat: add cloud function to add a new token to the database

--- app/src/main/java/com/gdg/foreground/services/sample/CloudFunctionsService.kt
+++ app/src/main/java/com/gdg/foreground/services/sample/CloudFunctionsService.kt
@@ -2,8 +2,9 @@ package com.gdg.foreground.services.sample
 
 import android.location.Location
 import android.util.Log
+import com.google.android.gms.tasks.Task
 import com.google.firebase.functions.FirebaseFunctions
-import timber.log.Timber
+import com.google.firebase.iid.FirebaseInstanceId
 
 
 /**
@@ -13,16 +14,35 @@ object CloudFunctionsService {
 
     fun updateLocation(location: Location) {
 
-Log.e("updateLocation", location.toString())        // Create the arguments to the callable function.
+        Log.e("updateLocation", location.toString())        // Create the arguments to the callable function.
         val data = HashMap<String, Any>()
         data["latitude"] = location.latitude
         data["longitude"] = location.longitude
 
         FirebaseFunctions.getInstance()
-                .getHttpsCallable("updateLocation")
-                .call(data)
-                .continueWith { task ->
-                    task.result.data as String
-                }
+            .getHttpsCallable("updateLocation")
+            .call(data)
+            .continueWith { task ->
+                task.result?.data as String
+            }
+    }
+
+    fun addToken(): Task<String> {
+        // Create the arguments to the callable function.
+
+        val data = java.util.HashMap<String, Any?>()
+        data["token"] = FirebaseInstanceId.getInstance().token
+
+        return FirebaseFunctions.getInstance()
+            .getHttpsCallable("addDeviceToken")
+            .call(data)
+            .continueWith { task ->
+                // This continuation runs on either success or failure, but if the task
+                // has failed then getResult() will throw an Exception which will be
+                // propagated down.
+                // Log.e("error viewholder_message",task.exception?.viewholder_message)
+                task.result?.data as String
+            }
+
     }
 }
--- app/src/main/java/com/gdg/foreground/services/sample/MainActivity.kt
+++ app/src/main/java/com/gdg/foreground/services/sample/MainActivity.kt
@@ -32,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addToken()
 
         start_button.setOnClickListener {
             permissionCheckAndStartServiceIfGranted()
